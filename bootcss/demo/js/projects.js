91e5cb54082d31d85e915c81def3788006cc4c98
sgpyvip <ahsgpy@126.com>
2015-07-15 17:47:23 +0800

    create project

---
+++ bootcss/demo/js/projects.js
@@ -0,0 +1,12 @@
+$(function(){
+
+var sitenavbar = "<div id=\"site-navbar\" style=\"position: absolute; top: -4px; left: -3px; border: 0; z-index: 2000;padding:0;margin:0;\">" +
+			"<a href=\"/\" title=\"返回首页\" style=\"background:none;\">" +
+				"<img src=\"images/return-back.png\" style=\"padding:0;margin:0;border:0; -webkit-box-shadow: none;-moz-box-shadow: none;box-shadow: none;\">"+
+			"</a>"+
+		"</div>";
+
+	$("body").append(sitenavbar);
+
+});
+
